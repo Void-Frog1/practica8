b510c2d4b386f43e2f6828ad60bce0efad6b061c
Void-Frog1 <146322102+Void-Frog1@users.noreply.github.com>
2023-11-15 10:56:58 +0300

    Create laba2.cpp

---
+++ laba2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    
+    int arr[5]{ 1,4,6,23,67 };
+    int a = 1;
+    for (int i = 0; i < 5; i++)  
+    {
+        a += arr[i];
+    }
+    cout << a << endl;
+    return 0;
+}
